eba2b2ce5cb1cab140042a8764b5f3f8caa325e3
asahi <asap1017@docomo.ne.jp>
2021-01-21 06:44:31 +0900

    エラー詳細を追加

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,6 +3,10 @@ class ErrorsController < ApplicationController
     def index
         @errors = Error.all
     end
+
+    def show
+        @error = Error.find(params[:id])
+    end
     
     def new
         @error = Error.new
